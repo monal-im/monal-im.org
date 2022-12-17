b9e2aa2061c7145739c86ae0171552976a7b1fc2
Wise <wisewebworks@outlook.com>
2022-12-17 13:48:37 +0100

    The main.js and footer.html files has been modified.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,101 @@
-"dark"===localStorage.getItem("pref-theme")?document.body.classList.add("dark"):"light"===localStorage.getItem("pref-theme")?document.body.classList.remove("dark"):window.matchMedia("(prefers-color-scheme: dark)").matches&&document.body.classList.add("dark");let menu=document.getElementById("menu");menu&&(menu.scrollLeft=localStorage.getItem("menu-scroll-position"),menu.onscroll=function(){localStorage.setItem("menu-scroll-position",menu.scrollLeft)}),document.querySelectorAll('a[href^="#"]').forEach((e=>{e.addEventListener("click",(function(e){e.preventDefault();var t=this.getAttribute("href").substr(1);window.matchMedia("(prefers-reduced-motion: reduce)").matches?document.querySelector(`[id='${decodeURIComponent(t)}']`).scrollIntoView():document.querySelector(`[id='${decodeURIComponent(t)}']`).scrollIntoView({behavior:"smooth"}),"top"===t?history.replaceState(null,null," "):history.pushState(null,null,`#${t}`)}))}));var mybutton=document.getElementById("top-link");window.onscroll=function(){document.body.scrollTop>800||document.documentElement.scrollTop>800?(mybutton.style.visibility="visible",mybutton.style.opacity="1"):(mybutton.style.visibility="hidden",mybutton.style.opacity="0")},document.getElementById("theme-toggle").addEventListener("click",(()=>{document.body.className.includes("dark")?(document.body.classList.remove("dark"),localStorage.setItem("pref-theme","light")):(document.body.classList.add("dark"),localStorage.setItem("pref-theme","dark"))})),document.querySelectorAll("pre > code").forEach((e=>{const t=e.parentNode.parentNode,o=document.createElement("button");function copyingDone(){o.innerHTML="Kopiert!",setTimeout((()=>{o.innerHTML="Kopieren"}),2e3)}o.classList.add("copy-code"),o.innerHTML="Kopieren",o.addEventListener("click",(t=>{if("clipboard"in navigator)return navigator.clipboard.writeText(e.textContent),void copyingDone();const o=document.createRange();o.selectNodeContents(e);const n=window.getSelection();n.removeAllRanges(),n.addRange(o);try{document.execCommand("copy"),copyingDone()}catch(e){}n.removeRange(o)})),t.classList.contains("highlight")?t.appendChild(o):t.parentNode.firstChild==t||("TABLE"==e.parentNode.parentNode.parentNode.parentNode.parentNode.nodeName?
-// table containing LineNos and code
-e.parentNode.parentNode.parentNode.parentNode.parentNode.appendChild(o):
-// code blocks not having highlight as parent class
-e.parentNode.appendChild(o))}));
+if (localStorage.getItem("pref-theme") === "dark") {
+    document.body.classList.add('dark');
+} else if (localStorage.getItem("pref-theme") === "light") {
+    document.body.classList.remove('dark')
+} else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.body.classList.add('dark');
+}
+
+let menu = document.getElementById('menu')
+if (menu) {
+    menu.scrollLeft = localStorage.getItem("menu-scroll-position");
+    menu.onscroll = function () {
+        localStorage.setItem("menu-scroll-position", menu.scrollLeft);
+    }
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+        var id = this.getAttribute("href").substr(1);
+        if (!window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
+            document.querySelector(`[id='${decodeURIComponent(id)}']`).scrollIntoView({
+                behavior: "smooth"
+            });
+        } else {
+            document.querySelector(`[id='${decodeURIComponent(id)}']`).scrollIntoView();
+        }
+        if (id === "top") {
+            history.replaceState(null, null, " ");
+        } else {
+            history.pushState(null, null, `#${id}`);
+        }
+    });
+});
+
+var mybutton = document.getElementById("top-link");
+window.onscroll = function () {
+    if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
+        mybutton.style.visibility = "visible";
+        mybutton.style.opacity = "1";
+    } else {
+        mybutton.style.visibility = "hidden";
+        mybutton.style.opacity = "0";
+    }
+};
+
+document.getElementById("theme-toggle").addEventListener("click", () => {
+    if (document.body.className.includes("dark")) {
+        document.body.classList.remove('dark');
+        localStorage.setItem("pref-theme", 'light');
+    } else {
+        document.body.classList.add('dark');
+        localStorage.setItem("pref-theme", 'dark');
+    }
+})
+
+document.querySelectorAll('pre > code').forEach((codeblock) => {
+    const container = codeblock.parentNode.parentNode;
+
+    const copybutton = document.createElement('button');
+    copybutton.classList.add('copy-code');
+    copybutton.innerHTML = '{{- i18n "code_copy" | default "copy" }}';
+
+    function copyingDone() {
+        copybutton.innerHTML = '{{- i18n "code_copied" | default "copied!" }}';
+        setTimeout(() => {
+            copybutton.innerHTML = '{{- i18n "code_copy" | default "copy" }}';
+        }, 2000);
+    }
+
+    copybutton.addEventListener('click', (cb) => {
+        if ('clipboard' in navigator) {
+            navigator.clipboard.writeText(codeblock.textContent);
+            copyingDone();
+            return;
+        }
+
+        const range = document.createRange();
+        range.selectNodeContents(codeblock);
+        const selection = window.getSelection();
+        selection.removeAllRanges();
+        selection.addRange(range);
+        try {
+            document.execCommand('copy');
+            copyingDone();
+        } catch (e) { };
+        selection.removeRange(range);
+    });
+
+    if (container.classList.contains("highlight")) {
+        container.appendChild(copybutton);
+    } else if (container.parentNode.firstChild == container) {
+        // td containing LineNos
+    } else if (codeblock.parentNode.parentNode.parentNode.parentNode.parentNode.nodeName == "TABLE") {
+        // table containing LineNos and code
+        codeblock.parentNode.parentNode.parentNode.parentNode.parentNode.appendChild(copybutton);
+    } else {
+        // code blocks not having highlight as parent class
+        codeblock.parentNode.appendChild(copybutton);
+    }
+});
